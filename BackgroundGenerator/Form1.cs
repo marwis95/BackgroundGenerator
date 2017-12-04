a542fd12576e533ae7744e4ac2dca3138e35ed34
Marek <marwis95@o2.pl>
2017-12-04 09:56:53 +0100

    Generowanie tekstu

--- BackgroundGenerator/Form1.cs
+++ BackgroundGenerator/Form1.cs
@@ -11,6 +11,7 @@ namespace BackgroundGenerator
 {
     public partial class Form1 : Form
     {
+        public int x = 0, y = 0; 
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,28 @@ namespace BackgroundGenerator
         {
 
             pictureBox1.BackgroundImage = Image.FromFile("tlo.png");
+
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = x + 10;
+            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(textBox1.Text, font, Brushes.Black, new Point(x, y));
+            this.pictureBox1.Image = image;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(textBox1.Text, font, Brushes.Black, new Point(x, y));
+            this.pictureBox1.Image = image;
 
         }
     }
