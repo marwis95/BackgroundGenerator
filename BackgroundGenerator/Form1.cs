ead68af4908b9652f18f57b5c675866ee0d9a52d
Marek <marwis95@o2.pl>
2017-12-04 10:37:42 +0100

    Przasuwanie napisu

--- BackgroundGenerator/Form1.cs
+++ BackgroundGenerator/Form1.cs
@@ -20,29 +20,63 @@ namespace BackgroundGenerator
         private void Form1_Load(object sender, EventArgs e)
         {
 
-            pictureBox1.BackgroundImage = Image.FromFile("tlo.png");
+            //pictureBox1.BackgroundImage = Image.FromFile("tlo.png");
+            pictureBox1.Image = Image.FromFile("tlo.png");
 
         }
 
-        
-
-        private void button1_Click(object sender, EventArgs e)
+        private void writeOnImage()
         {
-            x = x + 10;
-            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            pictureBox1.Image = Image.FromFile("tlo.png");
+           // var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
+            var image = new Bitmap(this.pictureBox1.Image, this.pictureBox1.Width, this.pictureBox1.Height);
             var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
             var graphics = Graphics.FromImage(image);
             graphics.DrawString(textBox1.Text, font, Brushes.Black, new Point(x, y));
             this.pictureBox1.Image = image;
+            
+
         }
+
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            var image = new Bitmap(this.pictureBox1.Width, this.pictureBox1.Height);
-            var font = new Font("TimesNewRoman", 25, FontStyle.Bold, GraphicsUnit.Pixel);
-            var graphics = Graphics.FromImage(image);
-            graphics.DrawString(textBox1.Text, font, Brushes.Black, new Point(x, y));
-            this.pictureBox1.Image = image;
+            writeOnImage();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            x = x + 1;
+            writeOnImage();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            x = x - 1;
+            writeOnImage();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            x = x + 10;
+            writeOnImage();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            x = x - 10;
+            writeOnImage();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image.Save("qwe.png", System.Drawing.Imaging.ImageFormat.Png); 
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
 
         }
     }
